494528292b6d67ba4be57e50d9bb1b706780c684
MauMakan348 <putraajip266@gmail.com>
2025-12-05 11:15:59 +0800

    msk2

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
+
   <footer>
       <div>
         <p>&copy;2025 Mengkuda.</p>
      </div>
   </footer>
-=======
-   <body>
-    <footer>@PRODISI 2025</footer>
-  </body>
->>>>>>> 
-</html>
--- includes/meta.php
+++ includes/meta.php
@@ -1,17 +1,10 @@
 <!DOCTYPE html>
 <html lang="id">
-<<<<<<< HEAD
+    
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman 1</title>
     <link rel="stylesheet" href="assets/CSS/style.css">
 </head>
-=======
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Halaman 1</title>
-    <link rel="stylesheet" href="aset/css/style.css" />
-  </head>
->>>>>>> 
+
