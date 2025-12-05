ab254f6f616bad91efdaa9de44a31f247f088f89
MauMakan348 <putraajip266@gmail.com>
2025-12-05 10:50:38 +0800

    msk1

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+  <footer>
+      <div>
+        <p>&copy;2025 Mengkuda.</p>
+     </div>
+  </footer>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<body class="beranda01">
+
+    <!-- banner atas -->
+    <header>
+      <h1>MENGKUDA</h1>
+    </header>
+    <!-- banner atas done -->
+
+    <!-- navigasi -->
+   <nav>
+      <input type="checkbox" id="burgir-menu"><label for="burgir-menu" class="burgir">&#9776</label>
+      <ul class="navi">
+        <li><input type="checkbox" id="burgir-menu"><label for="burgir-menu" class="noburgir">&times</label></li>
+        <li><a href="beranda.html">Beranda</a></li>
+        <li><a href="kuda balap.html">KudaBalap</a></li>
+        <li><a href="arena balap.html">ArenaBalap</a></li>
+        <li><a href="sejarah balap.html">CeritaBalap</a></li>
+      </ul>
+    </nav>
+    <!-- navigasi done -->
